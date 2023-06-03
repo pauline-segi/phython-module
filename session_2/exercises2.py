c641ff9ad13c6539a7057731851102a553555885
Pauline Segi <p.segi@outlook.com>
2023-06-03 13:02:00 +1000

    excercises 2 and 3

---
+++ session_2/exercises2.py
@@ -0,0 +1,26 @@
+# sara_has_helmet = False
+# rei_has_rope = False
+
+# if sara_has_helmet and rei_has_rope:
+#     print("Let's send it!")
+# elif sara_has_helmet and not rei_has_rope:
+#     print("Who's unprepared now, Rei??")
+# elif not sara_has_helmet and rei_has_rope:
+#     print("No way, my brain is my moneymaker!")
+# else:
+#     not sara_has_helmet and not rei_has_rope
+#     print("I guess let's just go hiking?")
+
+
+light_color = "Red"
+light_color = "Amber"
+light_color = "Green"
+car_detected = False
+
+if light_color == "Green" and car_detected == False:
+    print("Do nothing.")
+if light_color == "Amber" and car_detected == False:
+    print("Do nothing.")
+else: 
+    light_color == "Red" and car_detected == True
+    print("Flash!")
---
+++ session_2/exercises3.py
@@ -0,0 +1,35 @@
+# foods = [
+#     "orange",
+#     "apple",
+#     "banana",
+#     "strawberry",
+#     "grape",
+#     "blueberry",
+#     ["carrot","cauliflower","pumpkin"],
+#     "passionfruit",
+#     "mango",
+#     "kiwifruit"]
+
+
+# print(foods[0])
+# print(foods[2])
+# print(foods[-1])
+# print(foods[0:3])
+# print(foods[7:11])
+# print(foods[6][-1])
+
+
+line = [
+    ["Monica", "in my life"],
+    ["Erica", "by my side"],
+    ["Rita's", "all I need"],
+    ["Tina's", "what I see"],
+    ["Sandra", "in the sun"],
+    ["Mary", "having fun"],
+    ["Jessica", "here I am"]
+]
+
+song = "A little bit of"
+
+print(song,line[0:1])
+
